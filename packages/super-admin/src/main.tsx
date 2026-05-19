f00393d82b2b5fae8d37466cef42287844e13e4a
denomacha00 <denomacha00@gmail.com>
2026-05-19 16:46:15 +0300

    fix: remove /super basename from super admin router

--- packages/super-admin/src/main.tsx
+++ packages/super-admin/src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/super">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
